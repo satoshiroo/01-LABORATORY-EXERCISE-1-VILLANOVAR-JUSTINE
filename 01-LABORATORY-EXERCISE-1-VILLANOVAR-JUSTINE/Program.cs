73c001f3d3b9df204cc69e176c61b4756a4700b5
satoshiroo <jvillanovar6@gmail.com>
2025-08-16 16:22:16 +0800

    created a new class named StudentInfoClass, also declared new variables

--- 01-LABORATORY-EXERCISE-1-VILLANOVAR-JUSTINE/Program.cs
+++ 01-LABORATORY-EXERCISE-1-VILLANOVAR-JUSTINE/Program.cs
@@ -18,5 +18,18 @@ namespace _01_LABORATORY_EXERCISE_1_VILLANOVAR_JUSTINE
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+    }
+
+    public class StudentInfoClass
+    {
+        public delegate long DelegateNumber(long num);
+        public delegate string DelegateText(string text);
+
+        public string FirstName;
+        public string LastName;
+        public string MiddleName;
+        public string Address;
+        public string Program;
     }
 }
